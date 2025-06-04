5ee35e36edb0e50575cfe51acad45cfab38fc553
const-nishant <nishantspatil04@gmail.com>
2025-06-04 22:09:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div className="text-3xl font-bold underline bg-gray-400 text-blue p-4 border-2 border-red-500">
+      App
+    </div>
+  );
+};
